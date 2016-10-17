4745cd295a2d7dd3a7f6c5b6d897d2e0add50dc5
riesal <riesal@gmail.com>
2016-10-17 20:21:51 +0700

    Update rsbGetPromoPrice.py

--- rsbGetPromoPrice.py
+++ rsbGetPromoPrice.py
@@ -11,7 +11,13 @@
 # format result as restful, not file
 
 import requests, json
-# you may have to install requests module via: pip install 'requests[security]'
+# you may have to install requests module via: 
+# - apt-get install libffi libffi-dev
+# - OR if you are using Ubuntu: CFLAGS=-I/usr/include/libffi/include pip install pyOpenSSL
+# - OR if you are using CentOS: yum install libffi libffi-devel
+# - OR if you are using MacOS: brew install libffi
+# - OR, CFLAGS=$(pkg-config --cflags libffi) LDFLAGS=$(pkg-config --libs libffi) pip install pyOpenSSL
+# - pip install 'requests[security]'
 
 response = requests.get("https://test.httpapi.com/api/resellers/promo-details.json?auth-userid=your-reseller-id&api-key=your-key")
 response.raise_for_status()
