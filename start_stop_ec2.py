fc72563597c0fb18e9c7fa5fa8c33a6589e80091
riesal <riesal@gmail.com>
2018-07-20 23:24:12 +0700

    Delete start_stop_ec2.py

--- start_stop_ec2.py
+++
@@ -1,9 +0,0 @@
-# riesal[at]gmail[dot]com
-import boto3
-region = 'your-aws-region-id'
-instances = ['your-instance-id1','your-instance-id2']
-
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name=region)
-    ec2.start_instances(InstanceIds=instances)
-    print 'stopped your instances: ' + str(instances)
