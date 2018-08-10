e42d9ed072182d5c90f9e142a69352b2ca73d7da
riesal <riesal@gmail.com>
2018-08-10 11:28:04 +0700

    Update start_stop_ec2.py
    add finally..

--- aws_lambda/start_stop_ec2.py
+++ aws_lambda/start_stop_ec2.py
@@ -13,6 +13,8 @@ def lambda_handler(event, context):
       ec2.stop_instances(InstanceIds=instances)
   except ClientError as error:
       print 'Sorry, can not find some of instances, stopping aborted.'
+  finally:
+      print 'all of EC2 staging instances was stopped.'
 
   rds_conn = boto3.client('rds',region_name=region)
   for stg in rds_instances:
@@ -21,5 +23,5 @@ def lambda_handler(event, context):
           rds_conn.stop_db_instance(DBInstanceIdentifier=stg)
       except ClientError as error:
           print 'Sorry, i can not find RDS instance of ' + str(stg)
-
-  print 'all of rds staging instances was stopped.'
+      finally:
+          print 'all of rds staging instances was stopped.'
