5ea093895495fab5515e9840f3293d8f9fac2ab6
riesal <riesal@gmail.com>
2018-08-10 11:20:19 +0700

    Update start_stop_ec2.py
    simple handling exception with boto3 in aws lambda.

--- aws_lambda/start_stop_ec2.py
+++ aws_lambda/start_stop_ec2.py
@@ -1,9 +1,25 @@
 # riesal[at]gmail[dot]com
 import boto3
+from botocore.exceptions import ClientError # try-catch in python
+
 region = 'your-aws-region-id'
 instances = ['your-instance-id1','your-instance-id2']
+rds_instances = ['your-rds-instance-id1','your-rds-instance-id2']
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name=region)
-    ec2.start_instances(InstanceIds=instances)
-    print 'stopped your instances: ' + str(instances)
+  ec2 = boto3.client('ec2', region_name=region)
+  try:
+      print 'Trying to stop all staging EC2 instances' + str(instances)
+      ec2.stop_instances(InstanceIds=instances)
+  except ClientError as error:
+      print 'Sorry, can not find some of instances, stopping aborted.'
+
+  rds_conn = boto3.client('rds',region_name=region)
+  for stg in rds_instances:
+      try:
+          print 'Trying to stop RDS instance of ' + str(stg)
+          rds_conn.stop_db_instance(DBInstanceIdentifier=stg)
+      except ClientError as error:
+          print 'Sorry, i can not find RDS instance of ' + str(stg)
+
+  print 'all of rds staging instances was stopped.'
